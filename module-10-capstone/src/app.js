ac83fa7e6679d4e69d526bd2ed255540764bb4ef
tricia-stratpoint <triciamonique.cuaresma@stratpoint.com>
2026-05-07 15:00:26 +0800

    chore: add request logging with morgan middleware

--- module-10-capstone/src/app.js
+++ module-10-capstone/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const notesRouter = require('./routes/notes');
 const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/health', (req, res) => {
