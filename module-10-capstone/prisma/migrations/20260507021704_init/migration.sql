7e85b7b43af83ac5047767dce2c37bba639d4f87
tricia-stratpoint <triciamonique.cuaresma@stratpoint.com>
2026-05-07 10:43:04 +0800

    chore: initialize project scaffold

---
+++ module-10-capstone/prisma/migrations/20260507021704_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "tag" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
